e42f1b0d28fcf9c6b32d51a0b2877954dae7662b
masashi-aws <masashi00aws@gmail.com>
2026-07-10 12:44:25 +0900

    chore: point nonprod/dev state at tomario-tfstate-nonprod bucket
    State already migrated via terraform init -migrate-state (bucket
    created with versioning, encryption, and public access block enabled).
    This just brings main.tf in line with the already-migrated backend.

--- envs/nonprod/dev/main.tf
+++ envs/nonprod/dev/main.tf
@@ -13,7 +13,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "tomario-tfstate-shared-bucket"
+    bucket  = "tomario-tfstate-nonprod"
     key     = "dev/terraform.tfstate"
     region  = "ap-northeast-1"
     encrypt = true
